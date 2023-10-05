893099a9b1d3ff661ed3c76fb583ad8aa02dbe34
FrancescoDefulgentiis <49612706+FrancescoDefulgentiis@users.noreply.github.com>
2023-10-06 01:44:39 +0200

    Add files via upload
    added new prototypes for cam and mic capture, adjusted prototype for setupCall

--- lib.h
+++ lib.h
@@ -6,4 +6,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-void Setup_Call();
+void Setup_Call(const char* senderIp, int senderPort, const char* receiverIp, int receiverPort);
+int Mic_capture();
+int Cam_capture();
