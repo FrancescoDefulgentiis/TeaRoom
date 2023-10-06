833b666e0c6a1aa055e02fe8dd04cc5ea7363717
FrancescoDefulgentiis <49612706+FrancescoDefulgentiis@users.noreply.github.com>
2023-10-06 14:59:43 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -4,10 +4,12 @@
 #include <iostream>
 #include <unistd.h>
 #include <cstring>
+
 //call maker
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+
 //camera capture
 /*
 #include <opencv2/opencv.hpp>
@@ -15,7 +17,23 @@
 #include <opencv2/videoio/videoio.hpp>
 */
 
+//phonebook
+#include <vector>
+#include <fstream>
+
+#define PORT_NUMBER 42069
+
+// Structure to represent a friend's information
+struct Friend
+{
+  std::string name;
+  std::string ipv6Address;
+};
+
 void Setup_Call(std::string senderIp, int senderPort, std::string receiverIp, int receiverPort);
 int Mic_capture();
 std::string Get_Public_ipv6();
 //int Cam_capture();
+void addFriend(std::vector<Friend> &phonebook, const std::string &filename);
+void displayFriends(const std::vector<Friend> &phonebook);
+std::string getFriendIp(const std::vector<Friend> &phonebook, const std::string &friendName);
