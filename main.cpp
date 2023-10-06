09cc3f8824fbc4ccda8818c7f0d30162291187fb
FrancescoDefulgentiis <49612706+FrancescoDefulgentiis@users.noreply.github.com>
2023-10-06 14:59:30 +0200

    Update main.cpp
    added a menù with multiple choice, to add a friend or call one

--- main.cpp
+++ main.cpp
@@ -4,11 +4,63 @@ using namespace std;
 
 int main(){
   string MyIp = Get_Public_ipv6();//retrieve my public ipv6
-  
+
+  vector<Friend> phonebook;//define a vector of structs to store friends
+  string filename = "phonebook.txt";
+
+  // Load existing phonebook from the file (if any)
+  ifstream inputFile(filename);
+  if (inputFile){
+    string name, ipv6Address;
+    while (inputFile >> name >> ipv6Address){
+      Friend friendInfo;
+      friendInfo.name = name;
+      friendInfo.ipv6Address = ipv6Address;
+      phonebook.push_back(friendInfo);
+    }
+    inputFile.close();
+  }
+
+  int choice;
+  string friendName, friendIp;
+  do{
+    cout << "\nPhonebook Menu:" << endl;
+    cout << "1. Add a friend and save phonebook" << endl;
+    cout << "2. Display Friends" << endl;
+    cout << "3. Call a friend" << endl;
+    cout << "4. Exit" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice){
+    case 1:
+      addFriend(phonebook, filename);
+      break;
+    case 2:
+      displayFriends(phonebook);
+      cin.ignore();
+      getline(cin, friendName);
+      break;
+    case 3:
+      cout << "insert the name of the friend you want to call: ";
+      cin.ignore();
+      getline(cin, friendName);
+      friendIp = getFriendIp(phonebook, friendName);
+      Setup_Call(MyIp, PORT_NUMBER, friendIp, PORT_NUMBER);
+      break;
+    case 4:
+      cout << "Exiting program." << endl;
+      break;
+    default:
+      cout << "Invalid choice. Please try again." << endl;
+    }
+  } while (choice != 4);
+
+  return 0;
   //use a windows manager to create a window, and interact with it
   /*
   #code here where i get sender Ip(mine) and receiver Ip, also we agree on wich port to use
   */
   //when the interaction that causes the call to start happen, call Setup_Call to make it happen
-  Setup_Call(MyIp, 8888, "::1", 8889);
+  Setup_Call(MyIp, PORT_NUMBER, "::1", PORT_NUMBER);
 }
