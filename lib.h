128a2e18eb3e91a532243746ab4670f4a5ceb503
FrancescoDefulgentiis <49612706+FrancescoDefulgentiis@users.noreply.github.com>
2023-10-06 12:39:36 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
 #include <unistd.h>
+#include <cstring>
+//call maker
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+//camera capture
+/*
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/videoio/videoio.hpp>
+*/
 
-void Setup_Call(const char* senderIp, int senderPort, const char* receiverIp, int receiverPort);
+void Setup_Call(std::string senderIp, int senderPort, std::string receiverIp, int receiverPort);
 int Mic_capture();
-int Cam_capture();
+std::string Get_Public_ipv6();
+//int Cam_capture();
