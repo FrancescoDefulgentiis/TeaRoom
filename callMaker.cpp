02b273fbf8e0b865ce6aa534556605c223ac1f6f
FrancescoDefulgentiis <49612706+FrancescoDefulgentiis@users.noreply.github.com>
2023-10-06 12:35:46 +0200

    Update and rename callMaker.c to callMaker.cpp

--- callMaker.cpp
+++ callMaker.cpp
@@ -1,30 +1,31 @@
 #include "lib.h"
 
-void Setup_Call(const char* senderIp, int senderPort, const char* receiverIp, int receiverPort){
-  int sender_socket, receiver_socket;//file descriptors for receivend and sending messages
-  struct sockaddr_in6 sender_addr, receiver_addr;//predefined struct for sender and receiver addresses
+using namespace std;
+
+void Setup_Call(string senderIp, int senderPort, string receiverIp, int receiverPort){
+  int sender_socket, receiver_socket;             // File descriptors for sending and receiving messages
+  struct sockaddr_in6 sender_addr, receiver_addr; // Predefined struct for sender and receiver addresses
   socklen_t addr_len = sizeof(struct sockaddr_in6);
 
   // Create sockets
-  sender_socket = socket(AF_INET6, SOCK_DGRAM, 0);//AF_INET6 means IPv6, SOCK_DGRAM means UDP
+  sender_socket = socket(AF_INET6, SOCK_DGRAM, 0); // AF_INET6 means IPv6, SOCK_DGRAM means UDP
   receiver_socket = socket(AF_INET6, SOCK_DGRAM, 0);
-  if (sender_socket == -1 || receiver_socket == -1)//error handles
-  {
+  if (sender_socket == -1 || receiver_socket == -1){// Error handles
     perror("Socket creation failed");
     exit(EXIT_FAILURE);
   }
 
   // Configure sender address
-  memset(&sender_addr, 0, addr_len);//set all bytes of sender_addr to 0 to avoid garbage values
+  memset(&sender_addr, 0, addr_len); // Set all bytes of sender_addr to 0 to avoid garbage values
   sender_addr.sin6_family = AF_INET6;
-  sender_addr.sin6_port = htons(senderPort);//htons converts the port number to network byte order
-  inet_pton(AF_INET6, senderIp, &(sender_addr.sin6_addr));//converts the IP address from text to binary form
+  sender_addr.sin6_port = htons(senderPort);               // htons converts the port number to network byte order
+  inet_pton(AF_INET6, senderIp.c_str(), &(sender_addr.sin6_addr)); // Converts the IP address from text to binary form
 
   // Configure receiver address
   memset(&receiver_addr, 0, addr_len);
   receiver_addr.sin6_family = AF_INET6;
   receiver_addr.sin6_port = htons(receiverPort);
-  inet_pton(AF_INET6, receiverIp, &(receiver_addr.sin6_addr));
+  inet_pton(AF_INET6, receiverIp.c_str(), &(receiver_addr.sin6_addr));
 
   // Bind receiver socket to address
   if (bind(receiver_socket, (struct sockaddr *)&receiver_addr, addr_len) == -1){
@@ -33,7 +34,7 @@ void Setup_Call(const char* senderIp, int senderPort, const char* receiverIp, in
   }
 
   // Sending data from sender to receiver
-  char sender_message[] = "Hello, receiver!\0";//message to be sent
+  const char *sender_message = "Hello, receiver!\0";
   sendto(sender_socket, sender_message, strlen(sender_message), 0,
          (struct sockaddr *)&receiver_addr, addr_len);
 
@@ -41,7 +42,7 @@ void Setup_Call(const char* senderIp, int senderPort, const char* receiverIp, in
   char receiver_buffer[1024];
   recvfrom(receiver_socket, receiver_buffer, sizeof(receiver_buffer), 0,
            (struct sockaddr *)&sender_addr, &addr_len);
-  printf("Received message: %s\n", receiver_buffer);
+  cout << "Received message: " << receiver_buffer << endl;
 
   // Close sockets
   close(sender_socket);
